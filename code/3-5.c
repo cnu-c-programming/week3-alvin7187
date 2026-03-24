9feffe509137e0b0f66b77083d9b597639665dc1
alvin7187 <alvin060718@gmail.com>
2026-03-24 22:59:36 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+void my_sum(char types,int args, ...){
+    va_list ap;
+    va_start(ap,args);
+    int sum = 0;
 
+    switch(types){
+        case 'S':
+            for(int i = 0; i<args; i++){
+                printf("%s ",va_arg(ap, char*));
+            }
+            printf("\n");
+            break;
+        case 'C':
+            for(int i = 0; i<args; i++){
+                printf("%c",va_arg(ap, int));
+            }
+            printf("\n");
+            break;
+        case 'D':
+            for(int i = 0; i<args; i++){
+                sum += va_arg(ap, int);
+            }
+            printf("%d\n",sum);
+            break;
+    }
+    va_end(ap);
+}
+
+int main(){
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
+    
     return 0;
 }
