a36ab4c1441edd324a5939a4ac31013fb2449c11
alvin7187 <alvin060718@gmail.com>
2026-03-24 22:30:59 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdarg.h>
 
-int main() {
+int average(int args, ...){
+    int sum = 0;
+    va_list ap;
+    va_start(ap, args);
 
+    for(int i = 0; i < args; i++){
+        sum += va_arg(ap, int);
+    }
+
+    va_end(ap);
+
+    return sum/args;
+}
+
+int main(){
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
     return 0;
 }
